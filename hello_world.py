3079f9442448786a004c82af59b842fb37b12f8e
M1sterDonut <lhg@tuta.io>
2017-03-19 00:02:18 +0000

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,12 @@
+print ("Hello Python world!")
+message = "Hello Python world!"
+print (message)
+
+message = "Hello Python Crash Course world!"
+print (message)
+
+message_2 = "Come on, be my baby, come on"
+print (message_2)
+
+mesage = "Hello Python Crash Course reader!"
+print (mesage)
