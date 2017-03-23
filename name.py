bbbd651672b0c737967079c31a16e7d1ca13c853
M1sterDonut <lhg@tuta.io>
2017-03-23 23:46:04 +0000

    Add files via upload

---
+++ name.py
@@ -0,0 +1,28 @@
+name = "ada lovelace"
+print(name.title())
+name = "ADA LOVELACE"
+print(name.title())
+
+name = "Ada Lovelace"
+print(name.upper())
+print(name.lower())
+
+first_name = "bruce"
+last_name = "wayne"
+full_name = first_name + " " + last_name
+print (full_name.title())
+print("Hello, " + full_name.upper() + "!")
+message = "Hello, " + full_name.title() + "!"
+print (message)
+
+print ("\tPython")
+print ("Languages: Python C JavaScript")
+print ("Languages: \n\tPython \n\tC \n\tJavaScript")
+
+favorite_language = 'python '
+favorite_language = favorite_language.rstrip()
+print (favorite_language)
+print (favorite_language.rstrip())
+
+favorite_language = '  Python   '
+print (favorite_language.strip().upper())
