7f107622dd661f7fd3d2b085c3775311992cb494
M1sterDonut <lhg@tuta.io>
2017-03-31 23:37:43 +0100

    Add files via upload

---
+++ name_greetings.py
@@ -0,0 +1,9 @@
+names = ['johnny', 'don julio', 'morgan']
+print (names[0].title())
+print (names[1].title())
+print (names[2].title())
+
+message = ", you're my best friend!"
+print (names[0].title() + message)
+print (names[1].title() + message)
+print (names[2].title() + message)
