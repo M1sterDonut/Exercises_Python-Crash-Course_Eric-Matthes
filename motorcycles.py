74e5cb370b56ef1050d8ba757ad30a3aac3d782d
M1sterDonut <lhg@tuta.io>
2017-04-01 23:53:01 +0100

    Add files via upload

---
+++ motorcycles.py
@@ -0,0 +1,10 @@
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print (motorcycles)
+
+#replacing the first element
+motorcycles[0] = 'ducati'
+print (motorcycles)
+
+#adding elements - appending
+motorcycles.append('ducati')
+print (motorcycles)
