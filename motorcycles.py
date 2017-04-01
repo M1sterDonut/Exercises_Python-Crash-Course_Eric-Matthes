416c3ff682f5594187ab4700f0878629eaacadaa
M1sterDonut <lhg@tuta.io>
2017-04-01 23:56:16 +0100

    Add files via upload

--- motorcycles.py
+++ motorcycles.py
@@ -8,3 +8,17 @@ print (motorcycles)
 #adding elements - appending
 motorcycles.append('ducati')
 print (motorcycles)
+
+#adding to empty list
+motorcycles = []
+motorcycles.append('honda')
+motorcycles.append('yamaha')
+motorcycles.append('suzuki')
+
+print(motorcycles)
+
+#inserting new element
+motorcycles = ['honda', 'yamaha', 'suzuki']
+
+motorcycles.insert(0, 'ducati')
+print (motorcycles)
