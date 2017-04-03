4cca0c7df9a193078150de3651c07ceaf1386679
M1sterDonut <lhg@tuta.io>
2017-04-03 23:49:20 +0100

    Add files via upload

--- motorcycles.py
+++ motorcycles.py
@@ -48,3 +48,29 @@ print (popped_motorcycle)
 motorcycles = ['honda', 'yamaha', 'suzuki']
 last_owned = motorcycles.pop()
 print ('The last motorcycle I owned was a ' + last_owned.title() + '.')
+
+#Popping Items from any Position in a List
+motorcycles = ['honda', 'yamaha', 'suzuki']
+first_owned = motorcycles.pop(0)
+print('The first motorcycle I owned was a ' + first_owned.title()+ '.')
+
+#Removing an Item by Value
+motorcycles = ['honda', 'yamaha', 'suzuki', 'ducati']
+print (motorcycles)
+
+motorcycles.remove('ducati')
+print (motorcycles)
+
+#use remove() method to work with a value removed from list
+motorcycles = ['honda', 'yamaha', 'suzuki', 'ducati']
+print (motorcycles)
+
+too_expensive = 'ducati'
+motorcycles.remove(too_expensive)
+print(motorcycles)
+print("\nA " + too_expensive.title() + " is too expensive for me.")
+
+
+
+
+
