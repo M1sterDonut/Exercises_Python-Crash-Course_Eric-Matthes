00a9859435261d8b5e9c1be6e35acede344dfc5a
M1sterDonut <lhg@tuta.io>
2017-04-04 23:55:34 +0100

    Add files via upload

---
+++ guest_list.py
@@ -0,0 +1,13 @@
+#Try it yourself: lists, popping, deleting, etc.
+
+guest_list = ['the dread pirate roberts', 'ragnar lothbrok', 'heath ledger', 'jimi hendrix']
+print (guest_list)
+print ("") #How else do you get an empty line in the print?
+
+#3-4
+invitation_part1 = "Dear "
+invitation_part2 = ",\nYou're kindly invited for dinner.\nI look forward to seeing you,\nM1sterDonut\n"
+print (invitation_part1 + guest_list[0].title() + invitation_part2)
+print (invitation_part1 + guest_list[1].title() + invitation_part2)
+print (invitation_part1 + guest_list[2].title() + invitation_part2)
+print (invitation_part1 + guest_list[3].title() + invitation_part2)
