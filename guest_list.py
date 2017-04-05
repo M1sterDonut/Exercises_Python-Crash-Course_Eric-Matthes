5464ae654dcb8b238009c2b9823e5a87846ad1bb
M1sterDonut <lhg@tuta.io>
2017-04-05 23:51:47 +0100

    Add files via upload

--- guest_list.py
+++ guest_list.py
@@ -24,7 +24,6 @@ print (sorry_msg1 + guest_cant_come.title() + sorry_msg2)
 print (guest_list)
 
 #replacing name of guest with new person
-guest_list.append('enrique iglesias')
 guest_list.insert(2, 'al pacino')
 print (guest_list)
 
@@ -36,3 +35,60 @@ print (invitation_part3 + guest_list[0].title() + invitation_part4)
 print (invitation_part3 + guest_list[1].title() + invitation_part4)
 print (invitation_part3 + guest_list[2].title() + invitation_part4)
 print (invitation_part3 + guest_list[3].title() + invitation_part4)
+
+#3-6: 
+bigger_table = ", \nI'm happy to announce we've found a larger table.\n-MrD\n"
+print (guest_list[0].title() + bigger_table)
+print (guest_list[1].title() + bigger_table)
+print (guest_list[2].title() + bigger_table)
+print (guest_list[3].title() + bigger_table)
+
+guest_list.insert(0, 'rick')
+guest_list.insert(2, 'morty')
+guest_list.append('enrique iglesias')
+invitation_part5 = ", I'm looking forward to see you at our larger and much improved dining experience.\n-MrD\n"
+print (guest_list)
+print (guest_list[0].title() + invitation_part5)
+print (guest_list[1].title() + invitation_part5)
+print (guest_list[2].title() + invitation_part5)
+print (guest_list[3].title() + invitation_part5)
+print (guest_list[4].title() + invitation_part5)
+print (guest_list[5].title() + invitation_part5)
+print (guest_list[6].title() + invitation_part5)
+
+sorry_msg3 = "Dear guests,\nUnfortunately I will only be able to invite two guests to the party.\n-MrD\n"
+print (sorry_msg3)
+
+popped_guest1 = guest_list.pop(1)
+sorry_msg4 = "I'm sorry "
+sorry_msg5 = ", \nI can't invite you this time.\nI hope we'll get a chance to make up for this missed occasion soon.\n-MrD\n"
+print (sorry_msg4 + popped_guest1.title() + sorry_msg5)
+
+popped_guest2 = guest_list.pop(2)
+sorry_msg4 = "I'm sorry "
+sorry_msg5 = ", \nI can't invite you this time.\nI hope we'll get a chance to make up for this missed occasion soon.\n-MrD\n"
+print (sorry_msg4 + popped_guest2.title() + sorry_msg5)
+
+popped_guest3 = guest_list.pop(2)
+sorry_msg4 = "I'm sorry "
+sorry_msg5 = ", \nI can't invite you this time.\nI hope we'll get a chance to make up for this missed occasion soon.\n-MrD\n"
+print (sorry_msg4 + popped_guest3.title() + sorry_msg5)
+
+popped_guest4 = guest_list.pop(2)
+sorry_msg4 = "I'm sorry "
+sorry_msg5 = ", \nI can't invite you this time.\nI hope we'll get a chance to make up for this missed occasion soon.\n-MrD\n"
+print (sorry_msg4 + popped_guest4.title() + sorry_msg5)
+
+popped_guest5 = guest_list.pop(2)
+sorry_msg4 = "I'm sorry "
+sorry_msg5 = ", \nI can't invite you this time.\nI hope we'll get a chance to make up for this missed occasion soon.\n-MrD\n"
+print (sorry_msg4 + popped_guest5.title() + sorry_msg5)
+
+still_invited1 = "Dear "
+still_invited2 = ",\nJust to let you know: You're still invited.\nI look forward to meeting you.\n-MrD\n"
+print (still_invited1 + guest_list[0].title() + still_invited2)
+print (still_invited1 + guest_list[1].title() + still_invited2)
+
+del guest_list[0]
+del guest_list[0]
+print (guest_list)
