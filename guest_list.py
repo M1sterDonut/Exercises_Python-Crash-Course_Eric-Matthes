f9c816bd18865994dc7a8185b064c99633294c65
M1sterDonut <lhg@tuta.io>
2017-04-05 23:24:33 +0100

    Add files via upload

--- guest_list.py
+++ guest_list.py
@@ -11,3 +11,28 @@ print (invitation_part1 + guest_list[0].title() + invitation_part2)
 print (invitation_part1 + guest_list[1].title() + invitation_part2)
 print (invitation_part1 + guest_list[2].title() + invitation_part2)
 print (invitation_part1 + guest_list[3].title() + invitation_part2)
+
+guest_list = ['the dread pirate roberts', 'ragnar lothbrok', 'heath ledger', 'jimi hendrix']
+print (guest_list)
+
+#name of the guest who can't make it
+guest_cant_come = guest_list.pop(2)
+
+sorry_msg1 = "Unfortunately, "
+sorry_msg2 = " can't make it to the party."
+print (sorry_msg1 + guest_cant_come.title() + sorry_msg2)
+print (guest_list)
+
+#replacing name of guest with new person
+guest_list.append('enrique iglesias')
+guest_list.insert(2, 'al pacino')
+print (guest_list)
+
+#second set of invitations
+print ("")
+invitation_part3 = "Dear "
+invitation_part4 = ",\nI'm very happy to hear that you've decided to join us.\nSee you at the party tonight.\n- M1sterDonut\n"
+print (invitation_part3 + guest_list[0].title() + invitation_part4)
+print (invitation_part3 + guest_list[1].title() + invitation_part4)
+print (invitation_part3 + guest_list[2].title() + invitation_part4)
+print (invitation_part3 + guest_list[3].title() + invitation_part4)
