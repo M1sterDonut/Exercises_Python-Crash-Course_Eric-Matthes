4012ae0ca64e5030fdd15bf513923ec041d58a10
M1sterDonut <lhg@tuta.io>
2017-04-12 23:21:47 +0200

    Add files via upload

---
+++ magicians.py
@@ -0,0 +1,11 @@
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+	print (magician)
+
+#print msg to each magician
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+	print (magician.title() + ", that trick was awesome!")
+	print ("Can you do that again? Please, " +magician.title() + ", pleeease!\n")
+
+print ("Thanks guys, you were all fantastic!")
