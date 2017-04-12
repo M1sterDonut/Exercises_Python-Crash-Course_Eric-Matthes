dc7f8076f0209bed626caae39a7e93ae7105194f
M1sterDonut <lhg@tuta.io>
2017-04-12 23:22:06 +0200

    Add files via upload

---
+++ pizzas.py
@@ -0,0 +1,6 @@
+pizzas = ['pizza funghi', 'pizza hawaii', 'pizza parma']
+for pizza in pizzas:
+	print (pizza + "\n")
+for pizza in pizzas:
+	print ("I really like " + pizza.title() + "!\n")
+print ("Pizza = <3")
