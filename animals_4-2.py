4e3663e9893bde5bb942916d4d236658be5c6ff7
M1sterDonut <lhg@tuta.io>
2017-04-12 23:22:41 +0200

    Add files via upload

---
+++ animals_4-2.py
@@ -0,0 +1,6 @@
+animals = ['parrots', 'canaries', 'birdpersons']
+for animal in animals:
+	print (animal)
+for animal in animals:
+	print (animal.title() + " make great pets.\n")
+print ("They all have wings!")
