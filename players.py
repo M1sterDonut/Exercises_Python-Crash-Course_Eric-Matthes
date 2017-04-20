2030e5b488588c0a3b91f2c45447d2a3e24fc31c
M1sterDonut <lhg@tuta.io>
2017-04-20 23:55:13 +0100

    Add files via upload

--- players.py
+++ players.py
@@ -5,3 +5,10 @@ print (players[1:4])
 print (players[0:4])
 print (players[2:])
 print (players[-3:])
+
+#looping through a slice
+players = ['charles', 'martina', 'michael', 'florence', 'eli']
+
+print ("\nHere are the first three players on my team:")
+for player in players[:3]:
+	print (player.title())
