1152c9183b02d5448d84797278d01d49af919be7
M1sterDonut <lhg@tuta.io>
2017-04-23 00:26:20 +0000

    Add files via upload

--- pizzas.py
+++ pizzas.py
@@ -3,4 +3,20 @@ for pizza in pizzas:
 	print (pizza + "\n")
 for pizza in pizzas:
 	print ("I really like " + pizza.title() + "!\n")
-print ("Pizza = <3")
+print ("Pizza = <3\n")
+
+pizzas.append('pizza tonno')
+pizzas.append('pizza diavolo')
+print (pizzas)
+
+print ("\nThe first three pizzas on the list are:")
+for pizza in pizzas [:3]:
+	print (pizza.title())
+
+print ("\nThree pizzas from the middle of the list are:")
+for pizza in pizzas [1:4]:
+	print (pizza.title())
+
+print ("\nThe last three pizzas in the list are:")
+for pizza in pizzas [-3:]:
+	print (pizza.title())
