ccfe8c8a58a5fc447763992f2ea270387af2788a
M1sterDonut <lhg@tuta.io>
2017-04-29 23:44:13 +0100

    Add files via upload

---
+++ buffet.py
@@ -0,0 +1,13 @@
+buffet_foods = ('rice with basil pork', 'stamppot', 'hamburger', 'apple pie', 'coffee')
+print (buffet_foods)
+#use for loop to print items
+for buffet_food in buffet_foods:
+	print (buffet_food.title())
+
+#try modify
+#buffet_foods[1] = 'burrito'
+
+buffet_foods = ['rice with basil pork', 'burrito', 'cheeseburger', 'apple pie', 'coffee']
+print ('\nNew and improved menu:')
+for buffet_food in buffet_foods:
+	print (buffet_food.title())
