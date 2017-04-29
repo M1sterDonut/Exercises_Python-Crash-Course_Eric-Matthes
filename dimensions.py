8a947d0eec701b9215aab0a42a7224d60d8b89b5
M1sterDonut <lhg@tuta.io>
2017-04-29 23:43:54 +0100

    Add files via upload

--- dimensions.py
+++ dimensions.py
@@ -4,3 +4,14 @@ print (dimensions[1])
 
 for dimension in dimensions:
 	print (dimension)
+
+dimensions = (200, 50)
+print ("original dimension:")
+for dimension in dimensions:
+	print (dimension)
+
+dimensions = (400, 100)
+print ("\nModified dimensions:")
+for dimension in dimensions:
+	print (dimension)
+
