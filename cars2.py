ac1f8986f4b89815b287844cdebea24057ed5e10
M1sterDonut <lhg@tuta.io>
2017-04-30 23:44:09 +0100

    Add files via upload

---
+++ cars2.py
@@ -0,0 +1,14 @@
+cars = ['audi', 'bmw', 'subaru', 'toyota']
+
+for car in cars:
+	if car == 'bmw':
+		print (car.upper())
+	else:
+		print (car.title())
+
+car = 'bmw'
+car == 'bmw'
+
+car = 'Audi'
+car.lower() == 'audi'
+car 
