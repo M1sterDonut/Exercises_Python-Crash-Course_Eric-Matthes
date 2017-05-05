da3a306b26bfe5a791c945453b47f3e078d40983
M1sterDonut <lhg@tuta.io>
2017-05-05 20:55:33 +0100

    Add files via upload

--- toppings.py
+++ toppings.py
@@ -3,6 +3,4 @@ requested_topping = 'mushrooms'
 if requested_topping != 'anchovies':
 	print ("Hold the anchovies!")
 
-requested_toppings = ['mushrooms', 'onions', 'pineapple']
-'mushroom' in requested_toppings
-'pepperoni' in requested_toppings
+print (requested_topping == 'pepperoni')
