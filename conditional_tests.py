f4143b4ec28b01a65e0934cfcfeadb66c2f48e0f
M1sterDonut <lhg@tuta.io>
2017-05-06 12:05:25 +0100

    Add files via upload

---
+++ conditional_tests.py
@@ -0,0 +1,35 @@
+weather = 'sunny'
+print ("Is the weather sunny today? I predict True.")
+print (weather == 'sunny')
+
+print ("\nIs the weather rainy today? I predict False.")
+print (weather == 'rainy')
+
+food = 'gross'
+print ("\nIs the food gross today? I predict True.")
+print (food == 'gross')
+
+print ("\nIs the food tasty today? I predict False.")
+print (food == 'tasty')
+
+# using !=
+shop = 'empty'
+print ("\nIs the shop busy today? I predict True.")
+print (shop == 'busy')
+
+if shop != 'busy': 
+	print ("Oh no, I was wrong!")
+else: 
+	print ("Look at all these people!")
+
+shop = 'busy'
+print ("\nIs the shop busy today? I predict True.")
+print (shop == 'busy')
+
+if shop != 'busy': 
+	print ("Oh no, I was wrong!")
+else: 
+	print ("Look at all these people!")
+
+#to be continued
+
