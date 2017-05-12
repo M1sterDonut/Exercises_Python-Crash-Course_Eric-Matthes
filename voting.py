a53bc55f6aff91d62de13099e574a4e8a0cc0c91
M1sterDonut <lhg@tuta.io>
2017-05-12 23:12:44 +0100

    Add files via upload

---
+++ voting.py
@@ -0,0 +1,15 @@
+age = 19
+if age >= 18:
+	print (age, '?')
+	print ("You're old enough to vote!")
+	print ("Have you registered to vote yet?")
+
+age = 17
+if age >= 18:
+	print (age, '?')
+	print ("You're old enough to vote!")
+	print ("Have you registered to vote yet?")
+else:
+	print ("\n", age, '?')
+	print ("Sorry, you are too young to vote.")
+	print ("Please register to vote as soon as you turn 18!")
