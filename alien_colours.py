4639066ae547112b4987f99c18e81ce93fec4bd3
M1sterDonut <lhg@tuta.io>
2017-05-20 23:48:06 +0100

    Add files via upload

---
+++ alien_colours.py
@@ -0,0 +1,58 @@
+#5-3 Alien Colours #1
+
+alien_colour = 'green'
+
+if 'green' in alien_colour:
+	print ("You just earned 5 points!")
+
+alien_colour = 'blue'
+
+if 'green' in alien_colour:
+	print ("You just earned 5 points!")
+else:
+	print ("NO OUTPUT")
+
+#5-4 Alien Colours #2
+
+alien_colour = 'red'
+
+if 'green' in alien_colour:
+	print ("You just earned 5 points for shooting the alien")
+else:
+	print ("You just earned 10 points.")
+
+alien_colour = 'green'
+
+if 'green' in alien_colour:
+	print ("You just earned 5 points for shooting the alien!")
+else:
+	print ("You just earned 10 points.")
+
+#5-5 Alien Colours #3
+
+alien_colour = 'yellow'
+
+if 'green' in alien_colour:
+	print ("You earned 5 points!")
+elif 'yellow' in alien_colour:
+	print ("You earned 10 points!")
+elif 'red' in alien_colour:
+	print ("You earned 15 points!")
+
+alien_colour = 'green'
+
+if 'green' in alien_colour:
+	print ("You earned 5 points!")
+elif 'yellow' in alien_colour:
+	print ("You earned 10 points!")
+elif 'red' in alien_colour:
+	print ("You earned 15 points!")
+
+alien_colour = 'red'
+
+if 'green' in alien_colour:
+	print ("You earned 5 points!")
+elif 'yellow' in alien_colour:
+	print ("You earned 10 points!")
+elif 'red' in alien_colour:
+	print ("You earned 15 points!")
