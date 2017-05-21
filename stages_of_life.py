8a7baa7f74127b1ee7c7d86a07106eb8346a2f81
M1sterDonut <lhg@tuta.io>
2017-05-21 20:11:36 +0100

    Add files via upload

---
+++ stages_of_life.py
@@ -0,0 +1,19 @@
+age = 42
+
+if age < 2:
+	print (str(age) + " years? You're a baby!")
+
+elif age <= 3: 
+	print (str(age) + " years? You're a toddler!")
+
+elif age <= 12:
+	print (str(age) + " years? You're a kid!")
+
+elif age <= 19:
+	print (str(age) + " years? You're a teenager!")
+
+elif age <=64:
+	print (str(age) + " years? You're an adult!")
+
+else:
+	print (str(age) + " years? You're an elder!")
