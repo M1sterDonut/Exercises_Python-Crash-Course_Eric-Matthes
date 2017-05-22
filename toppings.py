3c3c0c2806a683f28f3d40573da49a39f6b0f8cd
M1sterDonut <lhg@tuta.io>
2017-05-22 23:16:18 +0100

    Add files via upload

--- toppings.py
+++ toppings.py
@@ -13,5 +13,48 @@ if 'pepperoni' in requested_toppings:
 if 'extra cheese' in requested_toppings:
 	print ("Adding extra cheese.")
 
-print ("\nFinished making your pizza!")
+print ("\nFinished making your pizza!\n")
 
+#Checking for Special Items
+
+requested_toppings = ['mushrooms', 'green peppers', 'extra cheese']
+
+for requested_topping in requested_toppings:
+	print ("Adding " + requested_topping + ".")
+
+print ("\nFinished making your pizza!\n")
+
+requested_toppings = ['mushrooms', 'green peppers', 'extra cheese']
+
+for requested_topping in requested_toppings:
+	if requested_topping == 'green peppers':
+		print ("Sorry, we are out of green pepper right now.")
+	else:
+		print ("Adding " + requested_topping + ".")
+
+print ("\nFinished making your pizza!\n")
+
+#Checking That a List Is Not Empty
+
+requested_toppings = []
+
+if requested_toppings:
+	for requested_topping in requested_toppings:
+		print ("Adding " + requested_topping + ".")
+	print ("\nFinished making your pizza!")
+else:
+	print ("Are you sure you want a plain pizza?\n")
+
+# Using Multiple Lists
+
+available_toppings = ['mushrooms', 'olives', 'green peppers', 'pepperoni', 'pineapple', 'extra cheese']
+
+requested_toppings = ['mushrooms', 'french fries', 'extra cheese']
+
+for requested_topping in requested_toppings:
+	if requested_topping in available_toppings:
+		print ("Adding " + requested_topping + ".")
+	else:
+		print ("Sorry, we don't have " + requested_topping + ".")
+
+print ("\nFinished making your pizza!")
