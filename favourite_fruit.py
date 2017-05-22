d8ca58b563b5d1d48da0b55b48da40299ec4ee28
M1sterDonut <lhg@tuta.io>
2017-05-22 23:16:34 +0100

    Add files via upload

---
+++ favourite_fruit.py
@@ -0,0 +1,18 @@
+favourite_fruits = ['water melon', 'pineapple', 'papaya', 'orange', 'banana']
+
+if 'water melon' in favourite_fruits:
+	print ("I have a hat in my home, made of watermelone!!")
+
+if 'pineapple' in favourite_fruits:
+	print ("I have pen, I have pineapple - uh - pen-pineapple-apple-pen!")
+
+if 'grapes' in favourite_fruits:
+	print ("Yummy, grapes!")
+else:
+	print ("No grapes... :(")
+
+if 'banana' in favourite_fruits:
+	print ("Bananaaaaa, potato-naaaa")
+
+if 'orange' in favourite_fruits:
+	print ("Hey Apple, it's me, Orange!")
